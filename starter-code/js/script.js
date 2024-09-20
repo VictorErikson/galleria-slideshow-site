76cacf9a3bda1d32975b3369a6f3062862d19a9c
VictorErikson <victoreriksson_93@hotmail.com>
2024-09-20 15:29:55 +0200

    fixade så indexpage hämtar all info från array

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -2,20 +2,47 @@
 import { art } from "./art.js";
 
 
+// Iterate over the art and create the HTML
 
-let btnViewImg = document.querySelector('.viewImg');
-let btnCloseImg = document.querySelector('.closeImg');
-let fullscreen = document.querySelector('.fullscreen');
-//Fullscreen
-btnViewImg.addEventListener('click', function() {
-    fullscreen.style.display = 'flex';
-    document.body.classList.add('noscroll');
+const slideshow = document.querySelector(".slideshow");
+
+art.forEach((art) => { 
+    const slide = document.createElement("section");
+    slide.classList.add("slide");
+
+    const link = document.createElement("a");
+    link.href = art.href;
+
+    const img = document.createElement("img");
+    img.src = art.imgFullScreen;
+    img.alt = art.painting;
+    img.classList.add("slideImg");
+
+    const gradient = document.createElement("div");
+    gradient.classList.add("gradient");
+
+    const slideText = document.createElement("div");
+    slideText.classList.add("slideText");
+
+    const paintingNameContainer = document.createElement("div");
+    paintingNameContainer.classList.add("paintingNameContainer");
+
+    const painting = document.createElement("h2");
+    painting.textContent = art.painting;
+    painting.classList.add("painting");
+
+    const painterContainer = document.createElement("div");
+    painterContainer.classList.add("painterContainer");
+
+    const painter = document.createElement("h3");
+    painter.textContent = art.painter;
+
+    painterContainer.append(painter);
+    paintingNameContainer.append(painting);
+    slideText.append(paintingNameContainer, painterContainer);
+    link.append(img, gradient, slideText);
+    slide.append(link);
+    slideshow.append(slide);
 });
-
-btnCloseImg.addEventListener('click', function() {
-    fullscreen.style.display = 'none';
-    document.body.classList.remove('noscroll');
-});
-
 
 
---
+++ starter-code/js/scriptGallery.js
@@ -0,0 +1,14 @@
+let btnViewImg = document.querySelector('.viewImg');
+let btnCloseImg = document.querySelector('.closeImg');
+let fullscreen = document.querySelector('.fullscreen');
+
+//Fullscreen
+btnViewImg.addEventListener('click', function() {
+    fullscreen.style.display = 'flex';
+    document.body.classList.add('noscroll');
+});
+
+btnCloseImg.addEventListener('click', function() {
+    fullscreen.style.display = 'none';
+    document.body.classList.remove('noscroll');
+});
